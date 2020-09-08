4af5fad4b5e313c2959528be88ae6d42054e7fdf
Eigminas Slavinskas <e.slavinskas@newcastle.ac.uk>
2020-09-08 13:12:51 +0200

    Update posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,33 +9,29 @@ const User = require("../../models/User");
 // @route POST api/posts
 // @desc Create a post
 // @access Private
-router.post(
-  "/",
-  [auth, [check("text", "text is required").not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    try {
-      const user = await User.findById(req.user.id).select("-password");
-
-      const newPost = Post({
-        text: req.body.text,
-        name: user.name,
-        avatar: user.avatar,
-        user: req.user.id,
-      });
-
-      const post = await newPost.save();
-      res.json(post);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Server error");
-    }
+router.post("/", [auth, [check("text", "text is required").not().isEmpty()]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    const newPost = Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    });
+
+    const post = await newPost.save();
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
 
 // @route GET api/posts/
 // @desc Get all posts
@@ -107,10 +103,7 @@ router.put("/like/:id", auth, async (req, res) => {
     const post = await Post.findById(req.params.id);
 
     // check if the post has already been liked
-    if (
-      post.like.filter((like) => like.user.toString() === req.user.id).length >
-      0
-    ) {
+    if (post.like.filter(like => like.user.toString() === req.user.id).length > 0) {
       return res.status(400).json({ msg: "Post already liked" });
     }
 
@@ -131,17 +124,12 @@ router.put("/unlike/:id", auth, async (req, res) => {
     const post = await Post.findById(req.params.id);
 
     // check if the post has already been liked
-    if (
-      post.like.filter((like) => like.user.toString() === req.user.id)
-        .length === 0
-    ) {
+    if (post.like.filter(like => like.user.toString() === req.user.id).length === 0) {
       return res.status(400).json({ msg: "Post has not yet been liked" });
     }
 
     // Get remove index
-    const removeIndex = post.like
-      .map((like) => like.user.toString())
-      .indexOf(req.user.id);
+    const removeIndex = post.like.map(like => like.user.toString()).indexOf(req.user.id);
 
     post.like.splice(removeIndex, 1);
     await post.save();
@@ -155,36 +143,32 @@ router.put("/unlike/:id", auth, async (req, res) => {
 // @route POST api/posts/comment/:id
 // @desc Comment on a post
 // @access Private
-router.post(
-  "/comment/:id",
-  [auth, [check("text", "text is required").not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    try {
-      const user = await User.findById(req.user.id).select("-password");
-      const post = await Post.findById(req.params.id);
-
-      const newComment = {
-        text: req.body.text,
-        name: user.name,
-        avatar: user.avatar,
-        user: req.user.id,
-      };
-
-      post.comments.unshift(newComment);
-
-      await post.save();
-      res.json(post.comments);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Server error");
-    }
+router.post("/comment/:id", [auth, [check("text", "text is required").not().isEmpty()]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    };
+
+    post.comments.unshift(newComment);
+
+    await post.save();
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
 
 // @route POST api/posts/comment/:id/:comment_id
 // @desc Delete a comment
@@ -194,9 +178,7 @@ router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
     const post = await Post.findById(req.params.id);
 
     // pull out comment
-    const comment = post.comments.find(
-      (comment) => comment.id === req.params.comment_id
-    );
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
 
     // make sure comment exists
     if (!comment) {
@@ -209,9 +191,7 @@ router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
     }
 
     // Get remove index
-    const removeIndex = post.comments
-      .map((comment) => comment.user.toString())
-      .indexOf(req.user.id);
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
 
     post.comments.splice(removeIndex, 1);
     await post.save();
